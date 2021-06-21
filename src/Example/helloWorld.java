579a2124be8dfd59888561f89856898b7ee1d20a
Xu Minghao <49871408+XuMingHao-1998@users.noreply.github.com>
2021-06-21 11:34:01 +0800

    add helloWorld

---
+++ src/Example/helloWorld.java
@@ -0,0 +1,12 @@
+package Example;
+
+/**
+ * Author: 徐明皓
+ * Date: 2021-06-21 11:04
+ * Description: <描述>
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
