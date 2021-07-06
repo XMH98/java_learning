7be5926dd74cdccb51298b2e9f1fdc90256d43e3
Xu Minghao <49871408+XuMingHao-1998@users.noreply.github.com>
2021-07-06 13:00:50 +0800

    ExtendsDemo

--- src/Exercise/extendsDemo/Demo.java
+++ src/Exercise/extendsDemo/Demo.java
@@ -9,8 +9,19 @@ public class Demo {
     public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat();
-
         dog.shout();
         cat.shout();
+
+        Animal dog1 = new Dog();
+        dog1.shout();
+
+        Animal dog2 = (Animal) dog;
+        dog2.shout();
+
+        Animal dog3 = dog;
+        dog3.shout();
+
+        dog1 = new Animal();
+        dog1.shout();
     }
 }
